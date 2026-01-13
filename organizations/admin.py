7e4a814f0081f2049341ed67cc4ea5ee733ca7ff
jitheesh <jitheeshdeveloper@gmail.com>
2026-01-13 22:52:21 +0530

    chore: register SaaS domain models in Django admin

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Organization, OrganizationMember
 
 # Register your models here.
+admin.site.register(Organization)
+admin.site.register(OrganizationMember)
